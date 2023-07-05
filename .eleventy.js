4441819cfacd4af4fb07de3eca3f8b418b0d61a8
Tim <tim3play3@gmail.com>
2023-07-05 18:06:08 +0200

    modified:   .eleventy.js 	modified:   package-lock.json 	modified:   package.json 	modified:   public/assets/blog/javatictactoe_img1.png 	modified:   src/_includes/article-snippet.njk 	modified:   src/_includes/article.njk 	modified:   src/_includes/base.njk 	modified:   src/_includes/footer.njk 	modified:   src/_includes/header.njk 	modified:   src/blog/2022-11-07-javatictactoe.md 	modified:   src/index.njk 	modified:   src/style.css

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
-    // Return your Object options:
-    return {
-      dir: {
-        input: "src",
-        output: "public"
-      }
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public"
     }
-  };
+  }
+};
--- src/blog/2022-11-07-javatictactoe.md
+++ src/blog/2022-11-07-javatictactoe.md
@@ -2,13 +2,20 @@
 title: JavaTicTacToe
 description: A simple TicTacToe game programmed in Java
 author: Tim Heidler
-date: '2022-11-07T15:45:35+01:00'
+date: "2022-11-07T15:45:35+01:00"
 tags:
   - post
   - featured
 image: /assets/blog/javatictactoe_img1.png
 imageAlt: picture of the program
 ---
+
 # JavaTicTacToe
 
-This program is a little game I created in order to learn Java
+A simple tic-tac-toe game written in Java. Features: 
+- Show current player 
+- Calculate winner
+
+I created this game in order to learn the basics of programming in Java.
+
+The game's source code as well as a build can be found on my GitHub. [https://github.com/timplay33/JavaTicTacToe](https://github.com/timplay33/JavaTicTacToe)
