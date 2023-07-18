bcda2e11b2e41f1508d6a4f717ebf626b6063c6a
Tim <tim3play3@gmail.com>
2023-07-18 18:01:46 +0200

    added robots.txt and sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
